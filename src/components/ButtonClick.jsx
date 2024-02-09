13bf7c9499cb4ecd0a143333affa7f45b111d12e
Chea Chanbo <cbo75920@gmail.com>
2024-02-09 21:11:51 +0700

    add new file

---
+++ src/components/ButtonClick.jsx
@@ -0,0 +1,6 @@
+function ButtonClick() {
+  const handleClick = (e) => (e.target.textContent = "OUCH! 😕");
+
+  return <button onClick={(e) => handleClick(e)}> Click me 😁</button>;
+}
+export default ButtonClick;
